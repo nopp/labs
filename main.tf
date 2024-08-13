90fa1f7d31363665c0bb5f9ca8c7796d4f6b645f
Carlos Augusto Malucelli <camalucelli@gmail.com>
2024-08-13 17:38:09 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-resource "null_resource" "example" {}
+data "null_data_source" "values" {
+  inputs = {
+    all_server_ips = concat(
+      "192.168.0.10/32",
+      "192.168.0.20/32,
+    )
+  }
+}
+
+resource "null_resource" "example" {
+  instances = data.null_data_source.values.outputs["all_server_ips"]
+}
