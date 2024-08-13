d9a161662448afce823f7feb6b391eee010be8ce
Carlos Augusto Malucelli <camalucelli@gmail.com>
2024-08-13 17:52:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 data "null_data_source" "values" {
   inputs = {
-    all_server_ips = concat(["a", "b"], ["c", "d"])
+    all_server_ips = "woooow"
   }
 }
 
 resource "null_resource" "example" {
-  
   triggers = {
-    instances = data.null_data_source.values.outputs["all_server_ips"]
+    id = data.null_data_source.values.outputs["all_server_ips"]
   }
   provisioner "local-exec" {
     command = "uname -a"
